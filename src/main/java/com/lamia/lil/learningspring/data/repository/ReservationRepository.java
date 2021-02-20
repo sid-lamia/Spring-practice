e99020869828767dd633941c9cd170c98579ca96
Lamia Siddiquee <siddiquee.lamia@gmail.com>
2021-02-20 14:27:27 -0500

    Aded service classes

--- src/main/java/com/lamia/lil/learningspring/data/repository/ReservationRepository.java
+++ src/main/java/com/lamia/lil/learningspring/data/repository/ReservationRepository.java
@@ -1,5 +1,7 @@
 package com.lamia.lil.learningspring.data.repository;
 
+import java.sql.Date;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.lamia.lil.learningspring.data.entity.Reservation;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long>{
+	
+	Iterable<Reservation> findReservationByReservationDate(Date date);
 
 }
