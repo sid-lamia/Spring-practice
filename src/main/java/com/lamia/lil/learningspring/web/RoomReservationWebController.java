88817dc345b61c8c993bc6f74d45889bc9d235e6
Lamia Siddiquee <siddiquee.lamia@gmail.com>
2021-02-21 15:44:27 -0500

    added controller and webpages

---
+++ src/main/java/com/lamia/lil/learningspring/web/RoomReservationWebController.java
@@ -0,0 +1,37 @@
+package com.lamia.lil.learningspring.web;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.lamia.lil.learningspring.business.domain.RoomReservation;
+import com.lamia.lil.learningspring.business.service.ReservationService;
+
+@Controller
+@RequestMapping("/reservations")
+public class RoomReservationWebController {
+	private final ReservationService reservationService;
+
+	@Autowired
+	public RoomReservationWebController(ReservationService reservationService) {
+		this.reservationService = reservationService;
+	}
+	
+	@GetMapping
+	public String getReservations(@RequestParam(value = "date", required = false)String dateString, Model model) {
+		Date date = DateUtils.createDateFromString(dateString);
+		List<RoomReservation> roomReservations = this.reservationService.getRoomReservationsForDate(date);
+		model.addAttribute("roomReservations", roomReservations);
+		return "reservations";
+		
+	}
+	
+	 
+
+}
